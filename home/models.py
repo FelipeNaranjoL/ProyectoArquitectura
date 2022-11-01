19832bab1d4effc67f8eeda619b3a513eb107ff6
FelipeNaranjo <fe.naranjo@duocuc.cl>
2022-11-01 19:59:24 -0300

    primer envio

---
+++ home/models.py
@@ -0,0 +1,64 @@
+from unittest.util import _MAX_LENGTH
+from django.db import models
+
+# Create your models here.
+
+class Proveedor(models.Model):
+    rutProveedor = models.CharField(max_length=10)
+    dvProveedor = models.CharField(max_length=1)
+    nombreProveedor = models.CharField(max_length=50)
+    fonoProveedor = models.CharField(max_length=20)
+    correoProveedor = models.EmailField(max_length=50)
+    direccionProveedor =  models.CharField(max_length=50)
+    
+    
+    def __str__(self):
+        return self.nombreProveedor
+    
+class Producto(models.Model):
+    idProducto = models.CharField(primary_key=True,serialize=False,max_length=10)
+    nombreProducto = models.CharField(max_length=50)
+    descripcionProducto = models.CharField(max_length=50)
+    precioProducto = models.IntegerField
+    proveedor = models.ForeignKey(Proveedor,on_delete=models.CASCADE)
+    
+    def __str__(self):
+        return self.nombreProducto
+
+
+# class Usuario(models.Model):
+#     nombreUsuario = models.TextField
+#     contraseña = models.
+#     usuarioRut
+
+
+# class Administrador(models.Model):
+#     rutAdmin = models.CharField(max_length=20)
+#     dvAdmin = models.CharField(max_length=1)
+#     nombreAdmin = models.CharField(max_length=50)
+#     apellidoAdmin = models.CharField(max_length=50)
+#     correoAdmin = models.EmailField(max_length=50)
+#     fonoAdmin = models.CharField(max_length=20)
+
+# class Cliente(models.Model):
+#     rutCliente = models.CharField(max_length=20)
+#     dvCliente = models.CharField(max_length=1)
+#     nombreCliente = models.CharField(max_length=50)
+#     apellidoCliente = models.CharField(max_length=50)
+#     fonoCliente = models.CharField(max_length=20)
+#     correoCliente = models.EmailField(max_length=50)
+#     direccionCliente = models.EmailField(max_length=100)
+#     nombreUsuarioCli = models.CharField(max_length=50)
+    
+# class Empleado(models.Model):
+#     rutEmpleado = models.CharField(max_length=20)
+#     dvEmpleado = models.CharField(max_length=1)
+#     nombreEmpleado = models.CharField(max_length=50)
+#     apellidoEmpleado = models.CharField(max_length=50)
+#     fonoEmpleado = models.CharField(max_length=20)
+#     correoEmpleado = models.EmailField(max_length=50)
+#     direccionEmpleado = models.EmailField(max_length=100)
+#     nombreUsuarioEmp = models.CharField(max_length=50)
+
+
+# aasd2
---
+++ home/views.py
@@ -0,0 +1,52 @@
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import UserCreationForm ,AuthenticationForm
+from django.contrib.auth.models import User
+from django.db import IntegrityError
+from django.contrib.auth import login, logout, authenticate
+
+# Create your views here.
+
+def home(request):
+    return render(request, 'home.html')
+
+def registrarse(request):
+    if request.method == 'GET': 
+        return render(request, 'registrarse.html',{
+            'form': UserCreationForm
+            })
+    else:
+        if request.POST ['password1'] == request.POST ['password2']:
+            try:
+                user = User.objects.create_user(username=request.POST['username'], password=request.POST['password1'])
+                user.save()
+                login(request,user)
+                return redirect('home')
+            except IntegrityError:
+                return render(request, 'registrarse.html',{
+                    'form': UserCreationForm,
+                    "error":  "El correo ya existe"
+                    })
+        return render(request, 'registrarse.html',{
+            'form': UserCreationForm,
+            "error":  "Las contraseñas no coinciden"
+            })
+
+def cerrarSession(request):
+    logout(request)
+    return redirect('/')
+
+def iniciarSession(request):
+    if request.method == "GET":
+        return render(request, 'iniciarSession.html',{
+        'form': AuthenticationForm
+        })
+    else:
+        user = authenticate(request, username=request.POST['username'], password=request.POST['password'])
+        if user is None:
+            return render(request, 'iniciarSession.html',{
+            'form': AuthenticationForm,
+            'error': 'Las credenciales son incorrectas, intentanuevamente'
+            })
+        else:
+            login(request,user)
+            return redirect('home')
